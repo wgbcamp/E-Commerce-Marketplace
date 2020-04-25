1e098112307285327478e0f1622017bafa308b32
Warren Brown <warrengbrn@gmail.com>
2020-04-25 12:18:45 -0400

    another attempt at path correction for static build file, removed build directory from client/.gitignore

---
+++ client/build/precache-manifest.6d177ecb5bf279c1ed56dc229fb50fce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076e71c53e4cc3f558e02be2f7af42ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06737de6509a7b8d0783",
+    "url": "/static/js/2.3421fde0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3421fde0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e48d072182a8d9b4c4e3",
+    "url": "/static/js/main.c47cfaa0.chunk.js"
+  },
+  {
+    "revision": "9310978cd63ae7fc2be6",
+    "url": "/static/js/runtime-main.90a7c0af.js"
+  }
+]);
